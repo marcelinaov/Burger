f486cd314b5af615fc4577ac1d973ccff9485a5f
marcelinaov <anaortiz@Anas-MacBook-Pro.local>
2019-05-13 21:59:09 -0600

    code added to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
 //Set up MYSQL connection
 var mysql = require('mysql');
 
+var connection;
+
+if ( process.env.JAWSDB_URL ) {
+    connection = mysql.createConnection( process.env.JAWSDB_URL );
+} else {
+    connection = mysql.createConnection( {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    } );
+};
 
     connection = mysql.createConnection({
         host: 'localhost',
